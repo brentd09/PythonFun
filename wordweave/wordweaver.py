783f33a208b827962a9f0f03c0bc154f608378d5
Brent Denny <brent.denny@lumifygroup.com>
2023-03-17 14:37:26 +1000

    weaver

--- wordweave/wordweaver.py
+++ wordweave/wordweaver.py
@@ -1,28 +1,73 @@
-import urllib.request
+import urllib.request, difflib
 
-def get_similar(word,word_list):
-    similar_word_list = []
-    desired_state = {'diff': 1 ,'same': word_size-1}
+def get_character_diff(test_word,target_word):
+    char_diff = 0
+    w1_len = len(test_word)
+    w2_len = len(target_word)
+    if w1_len != w2_len:
+        return -1
+    for x in range(w1_len):
+        char_diff += 1 if test_word[x] != target_word[x] else 0
+    return char_diff    
+
+def get_character_same(test_word,target_word):
+    char_same = 0
+    w1_len = len(test_word)
+    w2_len = len(target_word)
+    if w1_len != w2_len:
+        return -1
+    for x in range(w1_len):
+        char_same += 1 if test_word[x] == target_word[x] else 0
+    return char_same 
+
+def closest_matching(list1_words, list2_words):
+    best_same = 0
+    best_words = []
+    for word1 in list1_words:
+        for word2 in list2_words:
+            same = get_character_same(word1, word2)
+            if same > best_same:
+                best_same = same
+                best_words = [[word1, word2, same]]
+            elif same == best_same:
+                best_words.append([word1, word2, same])      
+    return best_words         
+       
+
+def one_letter_diff(word, word_list):
+    words_one_ltr_dif = []
     for single_word in word_list:
-        word_state = {'diff':0,'same':0}
-        for indx in range(word_size): 
-            if word[indx] == single_word[indx]:
-                word_state['same'] += 1
-            else:
-                word_state['diff'] += 1  
-        if word_state == desired_state:
-            similar_word_list.append(single_word)
-    return similar_word_list         
+        if get_character_diff(single_word,word) == 1:
+            words_one_ltr_dif.append(single_word)
+    return words_one_ltr_dif         
+
+def unique_words(words):
+    return sorted(list(set(words)))    
+
 
 word_initial = input('Enter the initial word: ')
 word_target = input('Enter the target word: ')
 web_contents = urllib.request.urlopen('http://www.mieliestronk.com/corncob_lowercase.txt')
-result = web_contents.readlines()
-word_list = [x.decode("utf-8").replace('\r\n','') for x in result]
-word_size = len(word_target)
-filtered_word_list = [x for x in word_list if len(x) == word_size]
+all_words = [x.decode("utf-8").replace('\r\n','') for x in web_contents.readlines()]
+same_len_words = [x for x in all_words if len(x) == len(word_target)]
 
-similar = get_similar(word_target,filtered_word_list)
-print(similar)
-for sim in similar:
-    print(get_similar(sim,filtered_word_list))
+word_list = one_letter_diff(word_initial,same_len_words)
+print(word_list)
+
+all_words = []
+for i in word_list:
+    new_list = one_letter_diff(i,same_len_words)
+    print ('all= ',all_words)
+    print([x for x in new_list if x not in word_list and x not in all_words])
+    all_words = unique_words(all_words + new_list)
+
+
+
+# get init and target words - complete
+# get list of same length words - complete
+# get a starting list of words that are 1 letter different from the target word - complete
+# get a starting list of words that are 1 letter different from the initial word - complete
+# work toward the middle of the chain by comparing the two lists and identifying words with greater similarity
+# work with similar content first to find a word ladder 
+# repeat this until a matching word is found in both top and bottom lists
+
